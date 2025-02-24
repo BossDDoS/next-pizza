a37048df444d2bb6b231c7f6b7536f5ac3252345
Maxim L <maksim.ldokov.97@mail.ru>
2025-02-24 22:54:26 +0400

    Added state manager, title scroll feature

--- components/shared/products-group-list.tsx
+++ components/shared/products-group-list.tsx
@@ -1,6 +1,9 @@
+'use client';
 import React from 'react';
+import { useIntersection } from 'react-use';
 import { ProductCard, Title } from './';
 import { cn } from '@/lib/utils';
+import { useCategoryStore } from '@/store/category';
 
 interface Props {
   title: string;
@@ -17,8 +20,22 @@ export const ProductsGroupList: React.FC<Props> = ({
   className,
   listClassName,
 }) => {
+  const setActiveCategoryId = useCategoryStore((state) => state.setActiveId);
+  const intersectionRef = React.useRef(null); // ссылка на блок для обработки
+  const intersection = useIntersection(intersectionRef, {
+    threshold: 0.4,
+  });
+
+  React.useEffect(() => {
+    if (intersection?.isIntersecting) {
+      // если в поле видимости
+      console.log(categoryId);
+      setActiveCategoryId(categoryId);
+    }
+  }, [categoryId, title, intersection?.isIntersecting]);
+
   return (
-    <div className={className}>
+    <div className={className} id={title} ref={intersectionRef}>
       <Title text={title} size='lg' className='font-extrabold mb-5' />
 
       <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
